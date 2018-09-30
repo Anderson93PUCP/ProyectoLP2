fadc173275464eb279f5c9ea1ec17cba6c3d5422
paucar97 <c.paucarpoma@pucp.pe>
2018-09-30 14:21:10 -0500

    se hizo algunos cambios de ventana administar comision

---
+++ Formularios/frmAdministrarComisionVendedores.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formularios
+{
+    public partial class frmAdministrarComisionVendedores : Form
+    {
+        public frmAdministrarComisionVendedores()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegresar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var v = MessageBox.Show("¿Desea confirmar el pago al vendedor por el monto de 742.23 S/.", "confirmacion", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            if(v == DialogResult.OK)
+            {
+                //Close();
+                //Se hace los cambios de estados d elas facturas 
+                // se cambia el estado de las facturas
+            }
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+    }
+}
